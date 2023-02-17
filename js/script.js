bb3380ae3adc1e789e31e434f1797bc11e2ee181
Eric Takman <erictakman@gmail.com>
2023-02-17 18:12:37 +0100

    Fix fetch
    Fetch from adviceslip was not working correctly, so this is now using a beer generator api instead

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
-let adviceID = document.querySelector('#adviceID')
-let randomAdvice = document.querySelector('#randomAdvice')
+const adviceID = document.querySelector("h2")
+const randomAdvice = document.querySelector("p")
+const getRandomQuoteButton = document.querySelector(".getRandomQuoteButton")
 
 const adviceData = async () => {
-	let data = await fetch('https://api.adviceslip.com/advice')
-	.then(response => response.json())
-	.then(data => data)
-
-	return data
+	return await fetch("https://random-data-api.com/api/v2/beers")
+		.then((response) => response.json())
+		.then((data) => {
+			adviceID.innerHTML = data.brand
+			randomAdvice.innerHTML = data.name
+			console.log(data)
+		})
 }
 
-adviceData().then(data => {
-	adviceID.innerText = "Advice ID: "
-	randomAdvice.innerText = data.slip.advice
-})
+adviceData()
+
+getRandomQuoteButton.addEventListener("click", () => {
+	console.log("click")
+	adviceData()
+});
+
